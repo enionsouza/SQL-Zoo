c01a9f0e43de7c14ccb90ca5dfb71bc216c91977
Enio N. de Souza <enionsouza@gmail.com>
2021-05-17 19:41:25 -0400

    Add answers to 'SELECT within SELECT'

--- answers.sql
+++ answers.sql
@@ -287,7 +287,121 @@ Peace	1
 Physics	1
 
 
+/*****************************************************************************/
+
+-- SELECT within SELECT Tutorial
+
+
+-- 1 Bigger than Russia
+SELECT name FROM world
+  WHERE population >
+      (SELECT population FROM world
+        WHERE name='Russia');
+
+-- 2 Richer than UK
+SELECT name FROM world
+  WHERE continent = 'Europe' 
+    AND gdp/population > 
+      (SELECT gdp/population FROM world
+        WHERE name = 'United Kingdom')
+
+-- 3 Neighbours of Argentina and Australia
+SELECT name, continent FROM world
+  WHERE continent IN
+    (SELECT DISTINCT continent FROM world
+      WHERE name IN ('Argentina', 'Australia'))
+        ORDER BY name;
+
+-- 4 Between Canada and Poland
+SELECT name, population FROM world
+  WHERE population > (SELECT population FROM world WHERE name = 'Canada')
+    AND population < (SELECT population FROM world WHERE name = 'Poland');
+
+-- 5 Percentages of Germany
+SELECT name, CONCAT(ROUND(100*population/(SELECT population FROM world WHERE name = 'Germany'), 0), '%') AS percentage FROM world WHERE continent = 'Europe';
+
+-- 6 Bigger than every country in Europe
+SELECT name FROM world
+  WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp IS NOT NULL)
+
+-- 7 Largest in each continent
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent = x.continent
+          AND area IS NOT NULL);
+
+-- 8 First country of each continent (alphabetically)
+SELECT DISTINCT continent, name FROM world x
+  WHERE name <= ALL (SELECT name FROM world y
+                      WHERE x.continent = y.continent);
+
+-- Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+-- 9 Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+SELECT name, continent, population FROM world
+  WHERE continent IN
+    (SELECT DISTINCT continent FROM world x
+      WHERE 25000000 >=
+        ALL(SELECT population FROM world y
+          WHERE x.continent = y.continent))
+
+-- 10 Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+SELECT name, continent FROM world x 
+  WHERE x.population/3 >=
+    ALL(SELECT population FROM world y
+      WHERE y.continent = x.continent
+        AND x.name != y.name)
 
 
 
 
+-- Nested SELECT Quiz (https://sqlzoo.net/wiki/Nested_SELECT_Quiz)
+
+-- 1. Select the code that shows the name, region and population of the smallest country in each region
+SELECT region, name, population FROM bbc x WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AND population>0)
+
+-- 2. Select the code that shows the countries belonging to regions with all populations over 50000
+SELECT name,region,population FROM bbc x WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
+
+-- 3. Select the code that shows the countries with a less than a third of the population of the countries around it
+SELECT name, region FROM bbc x
+ WHERE population < ALL (SELECT population/3 FROM bbc y WHERE y.region = x.region AND y.name != x.name)
+
+-- 4. Select the result that would be obtained from the following code:
+-- SELECT name FROM bbc
+--  WHERE population >
+--        (SELECT population
+--           FROM bbc
+--          WHERE name='United Kingdom')
+--    AND region IN
+--        (SELECT region
+--           FROM bbc
+--          WHERE name = 'United Kingdom')
+Table-D
+France
+Germany
+Russia
+Turkey
+
+
+-- 5. Select the code that would show the countries with a greater GDP than any country in Africa (some countries may have NULL gdp values).
+SELECT name FROM bbc
+ WHERE gdp > (SELECT MAX(gdp) FROM bbc WHERE region = 'Africa')
+
+
+-- 6. Select the code that shows the countries with population smaller than Russia but bigger than Denmark
+SELECT name FROM bbc
+ WHERE population < (SELECT population FROM bbc WHERE name='Russia')
+   AND population > (SELECT population FROM bbc WHERE name='Denmark')
+
+-- 7. Select the result that would be obtained from the following code:
+-- SELECT name FROM bbc
+--  WHERE population > ALL
+--        (SELECT MAX(population)
+--           FROM bbc
+--          WHERE region = 'Europe')
+--    AND region = 'South Asia'
+Table-B
+Bangladesh
+India
+Pakistan
